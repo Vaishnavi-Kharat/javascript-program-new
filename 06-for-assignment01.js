c646a3d4246ae0c33b189b254854792260e523b5
Vaishnavi-Kharat <vaishnavikharat2020@gmail.com>
2024-08-19 22:55:59 +0530

    06-for-assignment01.js
    06-for-assignment01.js

---
+++ 06-for-assignment01.js
@@ -0,0 +1,34 @@
+console.log(`======== number from 5 to 15 ========`);
+for (let index = 5; index<=15; index++) {
+    console.log(index);
+}
+
+console.log(`======== number from 50 to 40 ========`);
+for (let index = 50; index>=40; index--) {
+    console.log(index);
+}
+
+console.log(`======== First 15 odd number ========`);
+for (let index =1 ; index <=30; index=index+2) {
+    console.log(index);
+}
+
+console.log(`======== First 10 even number ========`);
+for (let index = 2; index<=20; index=index+2) {
+    console.log(index);
+}
+
+console.log(`======== Table of 5 ========`);
+for (let index =5; index<=50; index= index+5) {
+    console.log(index);
+}
+
+console.log(`======== Table of 10 ========`);
+for (let index =10; index<=100; index= index+10) {
+    console.log(index);
+}
+
+console.log(`========  Print the table of 10 in reverse order ========`);
+for (let index =100; index>=10; index=index-10) {
+    console.log(index);
+}
